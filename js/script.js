8ad1af373742e16503649ac04dac097c8b37379f
Vlad Fursa <fursav@gatech.edu>
2014-03-14 15:38:27 -0400

    Added equal height calculation

--- js/script.js
+++ js/script.js
@@ -132,6 +132,17 @@ ViewModel = function() {
         }
     };
 
+    self.getSomething = function() {
+    	console.log("here")
+    	url = "http://www.boardgamegeek.com/boardgame/125618/libertalia"
+    	$.getJSON("http://query.yahooapis.com/v1/public/yql?"+
+                "q=select%20*%20from%20html%20where%20url%3D%22"+
+                encodeURIComponent(url)+
+                "%22&format=xml'&callback=?", function(data){
+    			console.log(data)
+    	})
+    }
+
     self.getGameDetails = function (id) {
         url = 'http://www.boardgamegeek.com/xmlapi2/thing?id='+ id + "&stats=1"
         $.getJSON("http://query.yahooapis.com/v1/public/yql?" +
@@ -146,6 +157,8 @@ ViewModel = function() {
                 }
                 self.selectedGame(gdata);
                 console.log(gdata)
+                $('html, body').animate({scrollTop:0}, 'slow');
+                //equalHeight($(".columns-equal > div"));
             }
          });   
     }
@@ -212,3 +225,77 @@ ViewModel = function() {
         //this.get('', function() { this.app.runRoute('get', '#search/ ') });
     }).run();   
  };
+
+ function equalHeight(group) {
+	var tallest = 0;
+	group.each(function() {
+		var thisHeight = $(this).height();
+ 		console.log(thisHeight)
+		if(thisHeight > tallest) {
+			tallest = thisHeight;
+		}
+	});
+	group.height(tallest);
+}
+
+function OnImageLoad(evt) {
+    var img = evt.currentTarget;
+
+    // what's the size of this image and it's parent
+    var w = $(img).width();
+    var h = $(img).height();
+    var tw = $(img).parent().width();
+    var th = $(img).parent().height();
+	console.log(tw)
+	console.log(th)
+
+    // compute the new size and offsets
+    var result = ScaleImage(w, h, tw, th, false);
+
+    // adjust the image coordinates and size
+    img.width = result.width;
+    img.height = result.height;
+    $(img).css("left", result.targetleft);
+    $(img).css("top", result.targettop);
+}
+
+function ScaleImage(srcwidth, srcheight, targetwidth, targetheight, fLetterBox) {
+
+    var result = { width: 0, height: 0, fScaleToTargetWidth: true };
+
+    if ((srcwidth <= 0) || (srcheight <= 0) || (targetwidth <= 0) || (targetheight <= 0)) {
+        return result;
+    }
+
+    // scale to the target width
+    var scaleX1 = targetwidth;
+    var scaleY1 = (srcheight * targetwidth) / srcwidth;
+
+    // scale to the target height
+    var scaleX2 = (srcwidth * targetheight) / srcheight;
+    var scaleY2 = targetheight;
+
+    // now figure out which one we should use
+    var fScaleOnWidth = (scaleX2 > targetwidth);
+    if (fScaleOnWidth) {
+        fScaleOnWidth = fLetterBox;
+    }
+    else {
+       fScaleOnWidth = !fLetterBox;
+    }
+
+    if (fScaleOnWidth) {
+        result.width = Math.floor(scaleX1);
+        result.height = Math.floor(scaleY1);
+        result.fScaleToTargetWidth = true;
+    }
+    else {
+        result.width = Math.floor(scaleX2);
+        result.height = Math.floor(scaleY2);
+        result.fScaleToTargetWidth = false;
+    }
+    result.targetleft = Math.floor((targetwidth - result.width) / 2);
+    result.targettop = Math.floor((targetheight - result.height) / 2);
+
+    return result;
+}
